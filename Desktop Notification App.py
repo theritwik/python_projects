73e197f4c65022b405f57cd508b1f8bce971c0b4
Ritwik Singh <theritwiksingh@gmail.com>
2025-05-07 17:07:01 +0530

    Create Desktop Notification App.py

---
+++ Desktop Notification App.py
@@ -0,0 +1,11 @@
+//Desktop Notification App
+
+import os
+import time 
+
+REPEAT_INTERVAL = 3600 # Repeat frequency in seconds
+
+while True:
+  command = "osascript -e \'say \"Hey Harry drink water\"\'; osascript -e \'display alert \"Hey Harry, Drink water\"\'"
+  os.system(command)
+  time.sleep(REPEAT_INTERVAL)
